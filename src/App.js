5f9cb77c952608edbab30aff339ab76078c07445
Andrew Locke <alocke12992@gmail.com>
2018-03-07 11:35:13 -0700

    Show Items

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, name: "Learn Rails", complete: true },
+      {id: 2, name: "Learn React", complete: false },
+      {id: 3, name: "Learn Redux", complete: false },
+    ],
+  }
+
   render() {
+    const { todos } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ul>
+          { todos.map( (todo, i) => {
+            return <li key={todo.id}>{todo.name}</li>
+            })
+          }
+        </ul>
       </div>
-    );
+    )
   }
 }
 
