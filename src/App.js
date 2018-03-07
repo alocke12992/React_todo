6e6456d49113fbd508b26c39964b8bf708e98e63
Andrew Locke <alocke12992@gmail.com>
2018-03-07 11:40:48 -0700

    Adds sort items

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import logo from './logo.svg';
 class App extends Component {
   state = {
     todos: [
-      {id: 1, name: "Learn Rails", complete: true },
-      {id: 2, name: "Learn React", complete: false },
-      {id: 3, name: "Learn Redux", complete: false },
+      {id: 1, name: "Zebra", complete: true },
+      {id: 2, name: "Aebra", complete: false },
+      {id: 3, name: "Bebra", complete: false },
     ],
   }
+
+  byName = (x,y) => {
+    if (x.name > y.name )
+      return 1 
+    if (x.name  < y.name) 
+      return -1 
+    return 0 
+  } 
 
   render() {
     const { todos } = this.state
     return (
       <div>
         <ul>
-          { todos.map( (todo, i) => {
+          { todos.sort(this.byName).map( (todo, i) => {
             return <li key={todo.id}>{todo.name}</li>
             })
           }
